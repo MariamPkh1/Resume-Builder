77d46e635dea18176100c3216b9733873b533a23
akaki <gachavaakakia@gmail.com>
2026-05-07 13:59:15 +0400

    europass template added

--- CV-Builder-Back-v2/apps/cvs/pdf.py
+++ CV-Builder-Back-v2/apps/cvs/pdf.py
@@ -1,16 +1,11 @@
 from django.template.loader import render_to_string
 
 
-def render_cv_pdf(*, cv, watermark=False):
+def render_cv_pdf(*, cv, watermark=False, template="classic"):
     from weasyprint import HTML
-
-    html_string = render_to_string(
-        "cvs/pdf.html",
-        {
-            "cv": cv,
-            "watermark": watermark,
-        },
-    )
+    europass_templates = ["europass", "modern"]
+    template_name = "cvs/pdf_europass.html" if template in europass_templates else "cvs/pdf.html"
+    html_string = render_to_string(template_name, {"cv": cv, "watermark": watermark})
     return HTML(string=html_string).write_pdf()
 
 
